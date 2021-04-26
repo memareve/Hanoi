cc7e69b0ae1ff8d6b33727fd0be046d13a35da91
Мария Евдищенко <73089321+memareve@users.noreply.github.com>
2021-04-26 08:35:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-def moveTower(height, fromm, too, withh):
-    """Hanoi Towers feature"""
-    if height >= 1:
-        moveTower(height-1, fromm, withh, too)
-        moveDisk(fromm, too)
-        moveTower(height-1, withh, too, fromm)
+def moveTower(level, origin_s, goal_s, auxiliary_s):
+
+    if level >= 1:
+        moveTower(level-1, origin_s, auxiliary_s, goal_s)
+        moveDisk(origin_s, goal_s)
+        moveTower(level-1, auxiliary_s, goal_s, origin_s)
 
 
-def moveDisk(fp,tp):
-    print("moving disk from", fp, "to", tp)
+def moveDisk(from_s,to_s):
+    print("moving disk from", from_s, "to", to_s)
 
 
-n = int(input('Enter the number of rings: '))
-moveTower(n, "A", "B", "C")  # A - 1st kernel B - 2nd kernel C - 3nd kernel
-
+n = int(input('Enter the number of disks: '))
+moveTower(n, "A", "B", "C")  # A - 1st stick B - 2nd stick C - 3rd stick
